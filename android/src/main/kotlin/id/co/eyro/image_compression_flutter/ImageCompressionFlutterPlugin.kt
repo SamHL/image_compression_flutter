3bac432b65b3805b37d4210e0fa8ad1403fa3847
Samuel Hislop-Lynch <samuel_hislop-lynch@hotmail.com>
2025-03-01 11:59:25 +1000

    Update ImageCompressionFlutterPlugin.kt

--- android/src/main/kotlin/id/co/eyro/image_compression_flutter/ImageCompressionFlutterPlugin.kt
+++ android/src/main/kotlin/id/co/eyro/image_compression_flutter/ImageCompressionFlutterPlugin.kt
@@ -2,7 +2,7 @@ package id.co.eyro.image_compression_flutter
 
 import androidx.annotation.NonNull
 
-import io.flutter.embedding.engine.plugins.FlutterPlugin
+//import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler

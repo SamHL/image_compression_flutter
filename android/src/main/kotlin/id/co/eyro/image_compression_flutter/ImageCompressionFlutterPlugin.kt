d5cb7431516d66fa57c028049a5914b0aad7c65a
Samuel Hislop-Lynch <samuel_hislop-lynch@hotmail.com>
2025-03-01 12:15:02 +1000

    Update ImageCompressionFlutterPlugin.kt

--- android/src/main/kotlin/id/co/eyro/image_compression_flutter/ImageCompressionFlutterPlugin.kt
+++ android/src/main/kotlin/id/co/eyro/image_compression_flutter/ImageCompressionFlutterPlugin.kt
@@ -1,20 +1,15 @@
 package id.co.eyro.image_compression_flutter
 
 import androidx.annotation.NonNull
-
-//import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** ImageCompressionFlutterPlugin */
 class ImageCompressionFlutterPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
+  /// The MethodChannel that will handle the communication between Flutter and native Android
   private lateinit var channel : MethodChannel
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
